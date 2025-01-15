2030896b0188872d00b5c46732ca57f293d69073
EternalVision <eternalvision.ai@gmail.com>
2025-01-15 09:41:59 +0200

    Changed https to http

--- custom_utils.py
+++ custom_utils.py
@@ -10,7 +10,7 @@ import requests
 
 def api_screen(screen_id):
     # URL of the API endpoint
-    url = "https://127.0.0.1/api/screenstatus"
+    url = "http://127.0.0.1:5000/api/screenstatus"
     
     # Payload data
     payload = {
